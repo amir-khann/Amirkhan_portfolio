00f1287eaeb9eecab1f86ef10e563ca39e8a376c
amir khan <amiraup17@gmail.com>
2025-11-12 03:06:43 +0300

    Fix GitHub Pages routing: Add basename to BrowserRouter and fix NotFound links

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/Amirkhan_portfolio">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
